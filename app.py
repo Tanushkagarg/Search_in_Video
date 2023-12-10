28332b6d6be5a50f79e4e8961db8ee0e1acb2272
Krishna Bhargav N <144310727+KrishnaBhargavN@users.noreply.github.com>
2023-12-10 13:15:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def index():
         out_file = video.download(output_path=destination) 
 
         base, ext = os.path.splitext(out_file) 
-        new_file = base + '.mp3'
+        new_file = base + '.wav'
         os.rename(out_file, new_file) 
 
         print(yt.title + " has been successfully downloaded.")
