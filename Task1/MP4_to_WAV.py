269f51a107f56f146e1c9a3a3be8382d5304cdb2
Kaushik Kudchadkar <118542939+Kaushik2201@users.noreply.github.com>
2023-12-12 03:10:44 +0530

    Comment's added

--- Task1/MP4_to_WAV.py
+++ Task1/MP4_to_WAV.py
@@ -3,13 +3,16 @@ from pytube import YouTube
 
 def extract_audio(url, output_path='Output in WAV'):
     try:
-
+        # Fetching The video as an youtube object with the help of pytube module
         video = YouTube(url)
 
+        # Storing the Audio stream of the video
         audio_stream = video.streams.filter(only_audio=True).first()
 
+        # Downloading the audio on local storage
         audio_path = audio_stream.download(output_path=output_path)
 
+        # Converting the downloaded audio to .wav and saving it in Output folder
         wav_path = os.path.splitext(audio_path)[0] + ".wav"
         os.rename(audio_path, wav_path)
 
@@ -21,3 +24,5 @@ if __name__ == "__main__":
     video_url = input("Enter YouTube video URL: ")
     
     extract_audio(video_url)
+
+## This module cannot access age restricted youtube videos
