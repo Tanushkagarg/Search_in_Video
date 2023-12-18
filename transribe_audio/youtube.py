d43ccab8ed466afdb15bd2e0c2d2033031cd0e55
PramodC140 <146860445+PramodC140@users.noreply.github.com>
2023-12-18 15:15:16 +0530

    Create youtube.py

---
+++ transribe_audio/youtube.py
@@ -0,0 +1,22 @@
+
+
+
+
+from youtube_transcript_api import YouTubeTranscriptApi
+
+
+def transcribe_youtube(video_id):
+    """Transcribes audio from a YouTube video using its ID."""
+    srt = YouTubeTranscriptApi.get_transcript(video_id)
+    return srt
+
+
+def main():
+    """Prompts user for YouTube video ID, transcribes it, and prints the transcript."""
+    video_id = input("Enter the YouTube video ID: ")
+    transcript = transcribe_youtube(video_id)
+    print(f"\nTranscript:\n{transcript}")
+
+
+if __name__ == "__main__":
+    main()
